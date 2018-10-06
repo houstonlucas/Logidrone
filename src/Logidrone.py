ebcbffe13391a0f79121a067ca69415196ac937d
houstonlucas <houstonlucas@nevada.unr.edu>
2018-10-06 16:44:22 -0700

    Created skeleton of classes that will likely be needed.

---
+++ src/Logidrone.py
@@ -0,0 +1,23 @@
+class CircuitReader:
+    def __init__(self):
+        pass
+
+
+class DroneWriter:
+    def __init__(self):
+        pass
+
+
+class LogicGate:
+    def __init__(self):
+        pass
+
+
+class Tag:
+    def __init__(self):
+        pass
+
+
+class Sensor:
+    def __init__(self):
+        pass
