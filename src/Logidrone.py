d724baa2fb6e553b2f5d6c2d765019ccda367b84
houstonlucas <houstonlucas@nevada.unr.edu>
2018-10-06 17:22:00 -0700

    Added prefab_ids for most gates.

--- src/Logidrone.py
+++ src/Logidrone.py
@@ -4,6 +4,21 @@ class CircuitReader:
 
 
 class DroneWriter:
+    prefab_id_lookup = {
+        'BUTTON': '8bcf3ab6-fff7-43c5-ab51-ac18ad11e339',
+        'IF': '83d8bcdc-a7de-4e87-a453-6e242947ce1c',
+        'NOT': 'b5713ecf-dc95-4b92-b8d5-0dca8cc15ca8',
+        'AND': '2dc82d84-05c1-4874-ac2d-b69d4a02e88d',
+        'OR': 'afeb2d03-7820-4ad8-8604-3d91a8ecf0db',
+        'NOR': '79b0328f-a33c-4f9c-b386-1524a1ce9e91',
+        'NAND': '6e199749-a05b-4369-8bc3-2b614b5cf911',
+        'XOR': 'c5c58836-b49d-446d-8488-be144ab2e02e',
+        'XNOR': '2be149a7-dd24-4fb8-9f7f-e812c96ff497',
+        'SWITCH': '3ff7c4ef-1c36-41c7-9046-3395744f67f6',
+        'ONOFFSWITCH': '1951bd8f-47fd-477f-b355-c17a4ed9d769',
+        'LED': '25c66ac2-de8e-471f-9d5d-4df094a9ed27'
+    }
+
     def __init__(self):
         pass
 
